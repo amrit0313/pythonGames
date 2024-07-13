879677223fb0aa45a149cd1bfaf16c966bf74dbd
amrit0313 <amrtz0313@gmail.com>
2024-07-13 22:00:24 +0545

    loop added to RockPaper

--- Hangman.py
+++ Hangman.py
@@ -1,17 +1,29 @@
 import random
 
 wordList = ['try', 'unknown', 'shahrukh', 'brain', 'she', 'engineer']
-
+# index = 0
 choosenWord = random.choice(wordList)
+print(f"the word to guess is {choosenWord}")
+display = []
+for letters in choosenWord:
+    display += "_"
+print(display)
+endOfGame = False
+while not endOfGame:
+    guess = input("Guess the word: ").lower()
+    length = len(choosenWord)
 
-guess = input("Guess the word").lower()
+    # for x in choosenWord:
+    #     if x == guess:
+    #         display[index] = x
+    #     index += 1
+    # print(display)
+    for position in range(0, length):
+        letter = choosenWord[position]
+        if guess  == letter:
+            display[position] = letter
+    print(display)
 
-for letter in choosenWord:
-    if letter == guess:
-        print("Right")
-    else:
-        print("wrong")
-
-
-
-
+    if "_" not in display:
+        print("Damn!!! you guessed it right")
+        endOfGame = True
--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -24,26 +24,31 @@ my_list = [
 ---.__(___)
 """
 ]
-userChoice = int(
-    input("Take your choice, '0 for rock, 1 for paper and 2 for scissors': "))
-if (userChoice > 2):
-    print("invalid choose upto 2")
-randomNumber = random.randint(0, 2)
-computerChoice = my_list[randomNumber]
-if userChoice < 3 and userChoice >= 0:
-    print(f"your choice is\n {my_list[userChoice]}")
-print(f"computer choice is \n {computerChoice}")
-if userChoice > 3 or userChoice < 0:
-    print("input the valid number")
-elif userChoice == 0 and randomNumber == 2:
-    print("you won")
-elif randomNumber == 0 and userChoice == 2:
-    print("you lose")
-elif randomNumber > userChoice:
-    print("you lose ")
-elif userChoice > randomNumber:
-    print("you won")
-elif userChoice == randomNumber:
-    print("you made a boo-boo")
-else:
-    print("enter the valid statement")
+
+endOfGame = False
+while not endOfGame:
+    userChoice = int(
+        input("Take your choice, '0 for rock, 1 for paper and 2 for scissors and 3 to end the game': "))
+    if userChoice == 3:
+        endOfGame =True
+    if (userChoice > 2):
+        print("invalid choose upto 2")
+    randomNumber = random.randint(0, 2)
+    computerChoice = my_list[randomNumber]
+    if userChoice < 3 and userChoice >= 0:
+        print(f"your choice is\n {my_list[userChoice]}")
+    print(f"computer choice is \n {computerChoice}")
+    if userChoice > 3 or userChoice < 0:
+        print("input the valid number")
+    elif userChoice == 0 and randomNumber == 2:
+        print("you won")
+    elif randomNumber == 0 and userChoice == 2:
+        print("you lose")
+    elif randomNumber > userChoice:
+        print("you lose ")
+    elif userChoice > randomNumber:
+        print("you won")
+    elif userChoice == randomNumber:
+        print("you made a boo-boo")
+    else:
+        print("enter the valid statement")
