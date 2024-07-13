7f9dc76adcd96a02fc7e4da15003ebe974226131
amrit0313 <amrtz0313@gmail.com>
2024-07-13 22:53:33 +0545

    hangman completed

--- Hangman.py
+++ Hangman.py
@@ -1,18 +1,33 @@
-import random
+import random, hangmanArt, hangmanWords
+import os
 
-wordList = ['try', 'unknown', 'shahrukh', 'brain', 'she', 'engineer']
-# index = 0
+def clear():
+    # Check if the operating system is Windows
+    if os.name == 'nt':
+        os.system('cls')
+    # For Linux and macOS, os.name is 'posix'
+    else:
+        os.system('clear')
+
+print(hangmanArt.logo)
+wordList = hangmanWords.words
+index = 0
 choosenWord = random.choice(wordList)
 print(f"the word to guess is {choosenWord}")
+
 display = []
+
 for letters in choosenWord:
     display += "_"
 print(display)
+length = len(choosenWord)
+lives = 7
+
+
 endOfGame = False
 while not endOfGame:
     guess = input("Guess the word: ").lower()
-    length = len(choosenWord)
-
+    clear()
     # for x in choosenWord:
     #     if x == guess:
     #         display[index] = x
@@ -22,8 +37,18 @@ while not endOfGame:
         letter = choosenWord[position]
         if guess  == letter:
             display[position] = letter
+       
     print(display)
 
-    if "_" not in display:
+    if guess not in choosenWord:
+         lives -= 1
+         print(hangmanArt.hangman_stages[index])
+         print(f"{guess.upper()} isnt there on word, you lost a life buddy")
+         index +=1
+         if lives == 0:
+             print("hey hangman tied up, wanna give a new try")
+             endOfGame = True    
+if "_" not in display:
         print("Damn!!! you guessed it right")
         endOfGame = True
+   
---
+++ hangmanWords.py
@@ -0,0 +1,13 @@
+words = [
+    "cat", "dog", "bird", "fish", "lion", "tiger", "bear", "wolf", 
+    "mouse", "cow", "horse", "frog", "snake", "zebra", "whale", "shark",
+    "book", "table", "chair", "lamp", "pen", "pencil", "paper", "phone", 
+    "clock", "shirt", "shoe", "hat", "coat", "pants", "bag", "key",
+    "house", "car", "bike", "train", "bus", "plane", "boat", "ship",
+    "tree", "flower", "grass", "leaf", "bush", "forest", "desert", "river",
+    "sun", "moon", "star", "cloud", "rain", "snow", "wind", "storm",
+    "school", "park", "store", "bank", "library", "museum", "church", "hospital",
+    "apple", "banana", "grape", "orange", "peach", "berry", "melon", "kiwi",
+    "carrot", "onion", "potato", "tomato", "bean", "peas", "corn", "rice",
+    "happy", "sad", "angry", "tired", "excited", "bored", "scared", "brave"
+]
