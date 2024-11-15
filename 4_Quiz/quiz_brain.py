4c5767f5beb03b6d8f92438a71e33defcdb32e4d
amrit0313 <amrtz0313@gmail.com>
2024-11-15 20:55:51 +0545

    Quiz added

---
+++ 4_Quiz/quiz_brain.py
@@ -0,0 +1,30 @@
+class QuizBrain:
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list =q_list
+        self.score = 0
+
+
+    def still_has_question(self):
+        """returns true if there are more questions, and false if questions ended"""
+        return len(self.question_list)>self.question_number
+
+
+    def next_q(self):
+        """leads to next question, after answering one"""
+        current_question = self.question_list[self.question_number]
+        self.question_number+=1
+        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        """checks if user answers true or false"""
+        if user_answer.lower() == correct_answer.lower():
+            print("Hey!you got it right")
+            self.score = self.score+1
+        else:
+            print("You got wrong here")
+        print(f"correct answer is{correct_answer}")
+        print(f"Your score is {self.score}/{self.question_number}")
+
+
